ebb48f14dffb7bad49cf2a716d472c916e46eb89
Ashutosh Das <git.pyprism@gmail.com>
2016-04-28 11:41:32 +0600

    bye bye node

--- scrapper/bdjobs.js
+++ scrapper/bdjobs.js
@@ -1,6 +1,7 @@
 /**
  * Created by prism on 4/15/16.
  */
+/*
 var request = require('request'),
     cheerio = require('cheerio').
     async = require('async');
@@ -51,6 +52,60 @@ function  x () {
     })
 
 
+}
+
+x();*/
+var request = require('request-promise'),
+    cheerio = require('cheerio');
+
+
+function x() {
+    var url = 'http://joblist.bdjobs.com/jobsearch.asp?fcatId=8',
+    bunny = request.jar();
+    bunny.setCookie(request.cookie('JOBSRPP=40'), url);   //set 40 results per page
+
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'
+        },
+        jar: bunny
+    };
+
+    var urls = [];
+    var keywords = ['python', 'nodejs', 'node', 'laravel', 'django', 'node.js'];
+
+    request(options).then(function(html) {
+        $ = cheerio.load(html);
+        var res = $('.job_title_text a').get();
+        //console.log(res);
+        res.map(function(x) {
+            var option = {
+                url: 'http://joblist.bdjobs.com/' + x['attribs']['href'],
+                jar: bunny
+            };
+            request(option).then(function (body) {
+                $ = cheerio.load(body);
+                var text = $('.comp_wrapper').text();
+                //console.log(text);
+
+                keywords.map(function (key) {
+                    if(text.match(/key/ig)) {
+                        urls[key] = 'http://joblist.bdjobs.com/' + x['attribs']['href'];
+                        console.log(key + ' ' + 'http://joblist.bdjobs.com/' + x['attribs']['href']);
+                    }
+                })
+            }).catch(function (err) {
+                console.log(err);
+            })
+        })
+    }).catch(function (err) {
+        console.log(err);
+    }).finally(function () {
+        //console.log(urls);
+    });
+
+    console.log(urls);
 }
 
 x();
