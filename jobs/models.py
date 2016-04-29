0c443b591a9d36427dbd16887876a5bc09f151a5
Ashutosh Das <git.pyprism@gmail.com>
2016-04-29 19:58:07 +0600

    added new model

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Keyword(models.Model):
     keyword = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Job(models.Model):
+    url = models.URLField()
+
