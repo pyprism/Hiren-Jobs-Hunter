68670cb5de6d88f1b038fa2f95b7473e88f58ed0
Ashutosh Das <git.pyprism@gmail.com>
2016-05-01 18:51:02 +0600

    added final scrapper

--- jobs/scrapper.py
+++ jobs/scrapper.py
@@ -15,10 +15,11 @@ def send_message(job):
     try:
         requests.post("https://api.mailgun.net/v3/" + JSON_DATA['mailgun_domain'] + "/messages",
                       auth=("api", JSON_DATA['mailgun_key']),
-                      data={"from": "Job Alert <" + JSON_DATA['mailgun_from'] + ">",
+                      data={"from": "New Job Post <" + JSON_DATA['mailgun_from'] + ">",
                             "to": [JSON_DATA['mailgun_to']],
-                            "subject": "Yo Hiren!",
-                            "text": "Here is a list of post(s) that I got for you! " + job})
+                            "subject": "Job about: " + job['keyword'],
+                            "text": "Here is a new job post that I got for you! " + job['keyword'] +
+                                    ": " + job['link']})
     except Exception as e:
         print(job, e)
 
@@ -30,22 +31,24 @@ def bdjobs():
     bunny = soup.find_all(class_='job_title_text')
     jobs = []
     jobs_format = {'keyword': '', 'link': ''}
-    urls = Job.objects.all()
     for div in bunny:
         for anchor in div.find_all('a'):
-            url = 'http://joblist.bdjobs.com/' + anchor.get('href')
-            for obj in urls:  # filter duplicate url that are already send
-                if not url == obj.url:
-                    print('x')
-                    hiren = requests.get(url)
+            url = 'http://joblist.bdjobs.com/' + anchor.get('href')   # first get the link
+            if Job.objects.filter(url=url).exists() is False:  # check for duplicate url that are already scraped
+                    hiren = requests.get(url)  # get the job post
                     job_post = BeautifulSoup(hiren.text, 'html.parser')
-                    post = job_post.find_all(class_='comp_wrapper')
+                    post = job_post.find_all(class_='comp_wrapper')   # get the job contents
                     for keyword in keywords:
                         if re.findall('\\b' + keyword.keyword + '\\b', str(post), re.I):
                             jobs_format['keyword'] = keyword.keyword
                             jobs_format['link'] = url
                             jobs.append(jobs_format)
-                            Job.objects.create(url=url)
+                    Job.objects.create(url=url)
     return jobs
 
-print(bdjobs())
+
+def job_runner():
+    jobs = bdjobs()
+    if jobs:
+        for job in jobs:
+            send_message(job)
