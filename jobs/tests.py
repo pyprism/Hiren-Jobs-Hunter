03257cac0b8188a057b4b0d0c45e874fd116c8cb
Ashutosh Das <git.pyprism@gmail.com>
2016-05-01 18:51:45 +0600

    added model test

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
+from .models import Keyword, Job
 
-# Create your tests here.
+
+class ModelTest(TransactionTestCase):
+    reset_sequences = True
+
+    def setUp(self):
+        Job.objects.create(url="http://job.me")
+        Keyword.objects.create(keyword="Django")
+
+    def test_job_model(self):
+        job = Job.objects.all()
+        self.assertEqual(job.count(), 1)
+        self.assertEqual(job[0].url, "http://job.me")
+
+    def test_keyword_model(self):
+        keywords = Keyword.objects.all()
+        self.assertEqual(keywords.count(), 1)
+        self.assertEqual(keywords[0].keyword, 'Django')
