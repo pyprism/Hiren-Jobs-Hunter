f35922fc73dafecbaa4f336657d997b3d519ad82
Ashutosh Das <git.pyprism@gmail.com>
2017-04-11 19:41:01 +0600

    added scheduler

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .scrapper import job_runner
 
-# Create your views here.
+
+def scheduler(request):
+    """
+    view for cron based scheduler
+    :param request:
+    :return:
+    """
+    job_runner()
+    return HttpResponse("Hello bunny :D")
+
