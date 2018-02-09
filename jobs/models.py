a2d079a7285c3a882a789b10835108d8c19f3acf
Ashutosh Das <git.pyprism@gmail.com>
2018-02-09 11:18:38 +0600

    added timestamp

--- jobs/models.py
+++ jobs/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Keyword(models.Model):
     keyword = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Job(models.Model):
     url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
