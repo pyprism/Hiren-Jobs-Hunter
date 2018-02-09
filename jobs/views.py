4edef8783f48def8060b671982b47913d632a57f
Ashutosh Das <git.pyprism@gmail.com>
2018-02-09 11:52:00 +0600

    Added cleaner

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from .scrapper import job_runner
+from .models import Job
+from datetime import datetime, timedelta
 
 
 def scheduler(request):
@@ -10,4 +12,16 @@ def scheduler(request):
     """
     job_runner()
     return HttpResponse("Hello bunny :D")
+
+
+def cleaner(request):
+    """
+    Cron endpoint for db cleanup
+    :param request:
+    :return:
+    """
+    last_month = datetime.today() - timedelta(days=30)
+    job = Job.objects.filter(created_at__lte=last_month)
+    job.delete()
+    return HttpResponse("Bunny Nuked!")
 
